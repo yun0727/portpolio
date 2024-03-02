0dd0cc8fcee5200a9396c42987c396c7034e077d
yun <austru00@gmail.com>
2024-03-02 18:11:42 +0900

    navbar hover 색 변경

--- src/Routes/Components/Header.tsx
+++ src/Routes/Components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Nav = styled.div`
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
   position: fixed;
   width: 100%;
@@ -13,24 +13,42 @@ const Nav = styled.div`
   padding: 20px 60px;
 `;
 
-const Item = styled.li`
+const Item = styled.div`
   color: black;
   margin-right: 20px;
+  padding: 10px;
+  border-radius: 30px;
+  &:hover {
+    background-color: #5ab2ff;
+  }
   /* color: ${(props) => props.theme.white.darker}; */
 `;
+const Col = styled.div`
+  display: flex;
+`;
 
+const Name = styled.div`
+  color: #5ab2ff;
+  font-size: 30px;
+  font-weight: 800;
+`;
 function Header() {
   return (
     <Nav>
-      <Item>
-        <Link to="/"> Profile </Link>
-      </Item>
-      <Item>
-        <Link to="/skills">Skills</Link>
-      </Item>
-      <Item>
-        <Link to="/project">Project</Link>
-      </Item>
+      <Col>
+        <Name>🍀Jungyun's Protpolio🍀</Name>
+      </Col>
+      <Col>
+        <Item>
+          <Link to="/"> Profile </Link>
+        </Item>
+        <Item>
+          <Link to="/skills">Skills</Link>
+        </Item>
+        <Item>
+          <Link to="/project">Project</Link>
+        </Item>
+      </Col>
     </Nav>
   );
 }
